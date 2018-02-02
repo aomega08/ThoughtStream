cfbf82e19a22164dbc49ceef5399bad718cc854d
Francesco Boffa <fra.boffa@gmail.com>
2018-02-02 23:29:19 +0000

    💚 Fixed RSpec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -16,7 +16,8 @@ Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |f| require f }
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
-  config.use_transactional_tests = true
+  # config.use_transactional_tests = true
+  config.use_transactional_fixtures = true
 
   config.infer_spec_type_from_file_location!
 
