a63385530bfa3adca8fabeaff7176915b8cd77dd
Francesco Boffa <fra.boffa@gmail.com>
2018-02-11 20:36:33 +0000

    ✨ Generate slugs from Post Revisions

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,14 @@
 class Post < ApplicationRecord
+  extend FriendlyId
+
+  friendly_id :title, use: :history
+
   has_many :post_revisions
 
   delegate :title, to: :latest_revision
 
   def latest_revision
-    post_revisions.order(created_at: :desc).first
+    post_revisions.order(created_at: :desc).first || post_revisions[-1]
   end
 
   def content
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,4 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
+  it 'generates a slug from the its first revision' do
+    post = Post.new
+    post.post_revisions << PostRevision.new(title: 'Hello world', md_content: 'hello world')
+
+    post.valid?
+    expect(post.slug).to eq 'hello-world'
+  end
 end
